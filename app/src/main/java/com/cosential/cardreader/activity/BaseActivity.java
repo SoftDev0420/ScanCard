3e3d4b598fb1c40ce38e69506be31191f90881b6
SoftDev0420 <softdev0420@gmail.com>
2017-02-10 18:50:10 +0100

    Add layouts

--- app/src/main/java/com/cosential/cardreader/activity/BaseActivity.java
+++ app/src/main/java/com/cosential/cardreader/activity/BaseActivity.java
@@ -3,10 +3,23 @@ package com.cosential.cardreader.activity;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.cosential.cardreader.R;
 
 public class BaseActivity extends Activity {
+
+    protected TextView title;
+    protected LinearLayout topLeftButtonLayout, topRightButtonLayout;
+    protected Button topLeftButton, topRightButton;
+    protected ImageButton topLeftImageButton, topRightImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +32,74 @@ public class BaseActivity extends Activity {
         Window window = getWindow();
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    protected void setupNavigationBar() {
+        title = (TextView) findViewById(R.id.topBarTitle);
+        topLeftButtonLayout = (LinearLayout) findViewById(R.id.topLeftButtonLayout);
+        topLeftButton = (Button) findViewById(R.id.topBarLeftButton);
+        topLeftImageButton = (ImageButton) findViewById(R.id.topBarLeftImageButton);
+        topRightButtonLayout = (LinearLayout) findViewById(R.id.topRightButtonLayout);
+        topRightButton = (Button) findViewById(R.id.topBarRightButton);
+        topRightImageButton = (ImageButton) findViewById(R.id.topBarRightImageButton);
+    }
+
+    protected void setTitle(String text) {
+        title.setText(text);
+    }
+
+    protected void setTopLeftButtonImage(int imageResId) {
+        topLeftButton.setVisibility(View.GONE);
+        topLeftImageButton.setImageResource(imageResId);
+        topLeftImageButton.setVisibility(View.VISIBLE);
+    }
+
+    protected void setTopLeftButtonText(String text) {
+        topLeftButton.setText(text);
+        topLeftButton.setVisibility(View.VISIBLE);
+        topLeftImageButton.setVisibility(View.GONE);
+    }
+
+    protected void setTopLeftButtonVisibility(int visibility) {
+        topLeftButtonLayout.setVisibility(visibility);
+    }
+
+    protected void setTopRightButtonImage(int imageResId) {
+        topRightButton.setVisibility(View.GONE);
+        topRightImageButton.setImageResource(imageResId);
+        topRightImageButton.setVisibility(View.VISIBLE);
+    }
+
+    protected void setTopRightButtonText(String text) {
+        topRightButton.setText(text);
+        topRightButton.setVisibility(View.VISIBLE);
+        topRightImageButton.setVisibility(View.GONE);
+    }
+
+    protected void setTopRightButtonVisibility(int visibility) {
+        topRightButtonLayout.setVisibility(visibility);
+    }
+
+    protected void adjustTopBarLayout() {
+        final LinearLayout topBarLayout = (LinearLayout)findViewById(R.id.topBarLayout);
+        ViewTreeObserver vto = topBarLayout.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                topBarLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                int leftWidth = topLeftButtonLayout.getWidth();
+                int rightWidth = topRightButtonLayout.getWidth();
+                LinearLayout.LayoutParams leftLayoutParams = (LinearLayout.LayoutParams) topLeftButtonLayout.getLayoutParams();
+                LinearLayout.LayoutParams rightLayoutParams = (LinearLayout.LayoutParams) topRightButtonLayout.getLayoutParams();
+                if (leftWidth > rightWidth) {
+                    rightLayoutParams.width = leftWidth;
+                    topRightButtonLayout.setLayoutParams(rightLayoutParams);
+                }
+                else {
+                    leftLayoutParams.width = rightWidth;
+                    topLeftButtonLayout.setLayoutParams(leftLayoutParams);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/cosential/cardreader/activity/LoginActivity.java
+++ app/src/main/java/com/cosential/cardreader/activity/LoginActivity.java
@@ -1,14 +1,53 @@
 package com.cosential.cardreader.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.cosential.cardreader.R;
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+
+    ImageButton btnRemember;
+
+    Boolean bRemember = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnRemember = (ImageButton) findViewById(R.id.checkBox);
+        btnRemember.setOnClickListener(this);
+
+        Button btnLogin = (Button) findViewById(R.id.loginButton);
+        btnLogin.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginButton:
+
+                break;
+            case R.id.checkBox:
+                updateRememberStatus();
+                break;
+        }
+    }
+
+    ////////////////////////////////
+
+    public void updateRememberStatus() {
+        bRemember = !bRemember;
+        if (bRemember) {
+            btnRemember.setImageResource(R.drawable.ic_check_on);
+        }
+        else {
+            btnRemember.setImageResource(R.drawable.ic_check_off);
+        }
+    }
+
+    ////////////////////////////////
 }
